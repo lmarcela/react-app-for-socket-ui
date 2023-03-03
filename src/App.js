884396f8881ac7861a36a27d601af2b302b2b09a
Marcela Malaver <marcela9409@gmail.com>
2023-03-03 11:05:27 -0500

    ✨ configurar socket.io

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 import { MovieAdd } from "./components/MovieAdd";
 import { MovieList } from "./components/MovieList";
 
+const connectSocketServer = () => {
+  const socket = io.connect("http://localhost:8080", {
+    transports: ["websocket"],
+  });
+  return socket;
+};
+
 function App() {
+  const [socket] = useState(connectSocketServer());
+  const [online, setOnline] = useState(false);
+
+  useEffect(() => {
+    setOnline(socket.connected);
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('connect',()=>{
+      setOnline(true);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('disconnect',()=>{
+      setOnline(false);
+    });
+  }, [socket]);
+
   return (
     <div className="container">
       <div className="alert">
         <p>
           Service status:
-          <span className="text-success"> Online</span>
-          <span className="text-danger"> Offline</span>
+          {online ? (
+            <span className="text-success"> Online</span>
+          ) : (
+            <span className="text-danger"> Offline</span>
+          )}
         </p>
       </div>
       <h1>MovieNames</h1>
