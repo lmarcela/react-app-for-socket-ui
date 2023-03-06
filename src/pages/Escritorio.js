cd63435e4ee8c7d3784fa32543cb0141ce266401
Marcela Malaver <marcela9409@gmail.com>
2023-03-06 15:59:09 -0500

    ✨ Pantalla de escritorio

--- src/pages/Escritorio.js
+++ src/pages/Escritorio.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Typography, Button, Divider } from "antd";
+import { CloseCircleOutlined, RightOutlined } from "@ant-design/icons";
+
+const { Title, Text } = Typography;
 
 export const Escritorio = () => {
+  const salir = () => {};
+  const siguienteTicket = () => {
+    console.log("siguienteTicket");
+  };
+
   return (
-    <div>Escritorio</div>
-  )
-}
+    <>
+      <Row>
+        <Col span={20}>
+          <Title level={2}>Marcela</Title>
+          <Text>Usted está trabajando en el escritorio: </Text>
+          <Text type="success"> 5 </Text>
+        </Col>
+
+        <Col span={4} align="right">
+          <Button shape="round" danger type="primary" onClick={salir}>
+            <CloseCircleOutlined />
+            Salir
+          </Button>
+        </Col>
+      </Row>
+
+      <Divider />
+
+      <Row>
+        <Col>
+          <Text>Está atendiendo el ticket número: </Text>
+          <Text style={{ fontSize: 30 }} type="danger">
+            55
+          </Text>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col offset={18} span={6} align="right">
+          <Button onClick={siguienteTicket} shape="round" type="primary">
+            <RightOutlined />
+            Siguiente
+          </Button>
+        </Col>
+      </Row>
+    </>
+  );
+};
--- src/pages/Ingresar.js
+++ src/pages/Ingresar.js
@@ -1,70 +1,81 @@
 import React from "react";
-import { Button, Form, Input, InputNumber } from "antd";
+import { Button, Form, Input, InputNumber, Typography, Divider } from "antd";
 import { SaveOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
+const { Title, Text } = Typography;
 export const Ingresar = () => {
+  const history = useNavigate();
+
   const onFinish = (values) => {
     console.log("Success:", values);
+    history("/escritorio");
   };
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
   return (
-    <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 14,
-      }}
-      style={{
-        maxWidth: 600,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Nombre del asesor"
-        name="asesor"
-        rules={[
-          {
-            required: true,
-            message: "Por favor ingrese su nombre!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <>
+      <Title level={2}>Ingresar</Title>
+      <Text>Ingrese su nombre y número de escritorio</Text>
+      <Divider />
 
-      <Form.Item
-        label="Escritorio"
-        name="escritorio"
-        rules={[
-          {
-            required: true,
-            message: "Ingrese el número de escritorio!",
-          },
-        ]}
-      >
-        <InputNumber min={1} max={99} />
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 4,
-          span: 16,
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
         }}
+        wrapperCol={{
+          span: 14,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Button type="primary" htmlType="submit" shape="round">
-          <SaveOutlined />
-          Ingresar
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Nombre del asesor"
+          name="asesor"
+          rules={[
+            {
+              required: true,
+              message: "Por favor ingrese su nombre!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Escritorio"
+          name="escritorio"
+          rules={[
+            {
+              required: true,
+              message: "Ingrese el número de escritorio!",
+            },
+          ]}
+        >
+          <InputNumber min={1} max={99} />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 4,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit" shape="round">
+            <SaveOutlined />
+            Ingresar
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
