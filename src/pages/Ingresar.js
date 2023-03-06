65261613eecf4fa29579fc6d71a0f1d1e1a74e0a
Marcela Malaver <marcela9409@gmail.com>
2023-03-06 15:36:53 -0500

    ✨ Pantalla de ingresar

--- src/pages/Ingresar.js
+++ src/pages/Ingresar.js
@@ -1,7 +1,70 @@
-import React from 'react'
+import React from "react";
+import { Button, Form, Input, InputNumber } from "antd";
+import { SaveOutlined } from "@ant-design/icons";
 
 export const Ingresar = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
-    <div>Ingresar</div>
-  )
-}
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 14,
+      }}
+      style={{
+        maxWidth: 600,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Nombre del asesor"
+        name="asesor"
+        rules={[
+          {
+            required: true,
+            message: "Por favor ingrese su nombre!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Escritorio"
+        name="escritorio"
+        rules={[
+          {
+            required: true,
+            message: "Ingrese el número de escritorio!",
+          },
+        ]}
+      >
+        <InputNumber min={1} max={99} />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 4,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit" shape="round">
+          <SaveOutlined />
+          Ingresar
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
--- src/pages/RouterPage.js
+++ src/pages/RouterPage.js
@@ -3,57 +3,71 @@ import {
   UserOutlined,
   VideoCameraOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, theme } from "antd";
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { Cola } from "./Cola";
+import { CrearTicket } from "./CrearTicket";
+import { Escritorio } from "./Escritorio";
+import { Ingresar } from "./Ingresar";
 const { Header, Sider, Content } = Layout;
 
 export const RouterPage = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <Layout style={{height: '100vh'}}>
-      <Sider>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={[
-            {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "Ingresar",
-            },
-            {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "Cola",
-            },
-            {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "Crear Ticket",
-            },
-          ]}
-        />
-      </Sider>
-      <Layout className="site-layout">
-        <Header
-          style={{
-            padding: 0,
-            // background: colorBgContainer,
-          }}
-        >
-        </Header>
-        <Content
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-            // background: colorBgContainer,
-          }}
-        >
-          Content
-        </Content>
+    <Router>
+      <Layout style={{ height: "100vh" }}>
+        <Sider collapsedWidth={0} breakpoint="md">
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            items={[
+              {
+                key: "1",
+                icon: <UserOutlined />,
+                label: <Link to="/ingresar">Ingresar</Link>,
+              },
+              {
+                key: "2",
+                icon: <VideoCameraOutlined />,
+                label: <Link to="/cola">Tickets en cola</Link>,
+              },
+              {
+                key: "3",
+                icon: <UploadOutlined />,
+                label: <Link to="/crear">Crear tickets</Link>,
+              },
+            ]}
+          />
+        </Sider>
+        <Layout className="site-layout">
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+            <Routes>
+              <Route path="/ingresar" element={<Ingresar />}></Route>
+              <Route path="/cola" element={<Cola />}></Route>
+              <Route path="/crear" element={<CrearTicket />}></Route>
+              <Route path="/escritorio" element={<Escritorio />}></Route>
+              {/* <Route path="*" element={<Navigate to="/ingresar" />} /> */}
+            </Routes>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </Router>
   );
 };
