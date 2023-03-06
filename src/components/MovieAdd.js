cf438459ad28e82b322bea18eaec0c63391e9849
Marcela Malaver <marcela9409@gmail.com>
2023-03-06 09:34:40 -0500

    ✨ Agregar banda usando el context

--- src/components/MovieAdd.js
+++ src/components/MovieAdd.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { useSocket } from "./hooks/useSocket";
+import React, { useContext, useState } from "react";
+import { SocketContext } from "../context/SocketContext";
 
 export const MovieAdd = () => {
   const [value, setValue] = useState("");
-  const { socket } = useSocket("http://localhost:8080");
+  const { socket } = useContext(SocketContext)
 
   const onSubmit = (ev) => {
     ev.preventDefault();
