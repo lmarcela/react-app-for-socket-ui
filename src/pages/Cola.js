f4903ef7bbe3b1ed223d66112a5f8f89118b17ed
Marcela Malaver <marcela9409@gmail.com>
2023-03-07 11:59:03 -0500

    ✨ Actualizar la cola de tickets en tiempo real

--- src/pages/Cola.js
+++ src/pages/Cola.js
@@ -1,49 +1,59 @@
 import { Card, Col, Divider, List, Row, Tag, Typography } from "antd";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { SocketContext } from "../context/SocketContext";
 import { useHideMenu } from "../hooks/useHideMenu";
 
 const { Title, Text } = Typography;
 
-const data = [
-  {
-    ticketNo: 33,
-    escritorio: 3,
-    agente: "Marcela Malaver",
-  },
-  {
-    ticketNo: 34,
-    escritorio: 4,
-    agente: "Yessica Gómez",
-  },
-  {
-    ticketNo: 35,
-    escritorio: 5,
-    agente: "Jeisson Malaver",
-  },
-  {
-    ticketNo: 36,
-    escritorio: 3,
-    agente: "Marcela Malaver",
-  },
-  {
-    ticketNo: 37,
-    escritorio: 3,
-    agente: "Marcela Malaver",
-  },
-  {
-    ticketNo: 38,
-    escritorio: 2,
-    agente: "Yessica Gómez",
-  },
-  {
-    ticketNo: 39,
-    escritorio: 5,
-    agente: "Jeisson Malaver",
-  },
-];
+// const data = [
+//   {
+//     ticketNo: 33,
+//     escritorio: 3,
+//     agente: "Marcela Malaver",
+//   },
+//   {
+//     ticketNo: 34,
+//     escritorio: 4,
+//     agente: "Yessica Gómez",
+//   },
+//   {
+//     ticketNo: 35,
+//     escritorio: 5,
+//     agente: "Jeisson Malaver",
+//   },
+//   {
+//     ticketNo: 36,
+//     escritorio: 3,
+//     agente: "Marcela Malaver",
+//   },
+//   {
+//     ticketNo: 37,
+//     escritorio: 3,
+//     agente: "Marcela Malaver",
+//   },
+//   {
+//     ticketNo: 38,
+//     escritorio: 2,
+//     agente: "Yessica Gómez",
+//   },
+//   {
+//     ticketNo: 39,
+//     escritorio: 5,
+//     agente: "Jeisson Malaver",
+//   },
+// ];
 
 export const Cola = () => {
   useHideMenu(true);
+  const { socket } = useContext(SocketContext);
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(() => {
+    socket.on("assigned-ticket", (tickets) => {
+      setTickets(tickets);
+    });
+    return () => socket.off("assigned-ticket");
+  }, [socket]);
 
   return (
     <>
@@ -51,17 +61,20 @@ export const Cola = () => {
       <Row>
         <Col span={12}>
           <List
-            dataSource={data.slice(0, 3)}
-            renderItem={(item) => (
+            dataSource={tickets.slice(0, 3)}
+            renderItem={(ticket) => (
               <List.Item>
                 <Card
                   style={{ width: 300, marginTop: 16 }}
                   actions={[
-                    <Tag color="volcano"> {item.agente} </Tag>,
-                    <Tag color="magenta"> Escritorio: {item.escritorio} </Tag>,
+                    <Tag color="volcano"> {ticket.asesor} </Tag>,
+                    <Tag color="magenta">
+                      {" "}
+                      Escritorio: {ticket.escritorio}{" "}
+                    </Tag>,
                   ]}
                 >
-                  <Title> No. {item.ticketNo}</Title>
+                  <Title> No. {ticket.number}</Title>
                 </Card>
               </List.Item>
             )}
@@ -71,17 +84,17 @@ export const Cola = () => {
         <Col span={12}>
           <Divider> Historial </Divider>
           <List
-            dataSource={data.slice(3)}
-            renderItem={(item) => (
+            dataSource={tickets.slice(3)}
+            renderItem={(ticket) => (
               <List.Item>
                 <List.Item.Meta
-                  title={`Ticket No. ${item.ticketNo}`}
+                  title={`Ticket No. ${ticket.number}`}
                   description={
                     <>
                       <Text type="secondary">En el escritorio: </Text>
-                      <Tag color="magenta"> {item.ticketNo} </Tag>
+                      <Tag color="magenta"> {ticket.escritorio} </Tag>
                       <Text type="secondary"> Agente: </Text>
-                      <Tag color="volcano"> {item.agente} </Tag>
+                      <Tag color="volcano"> {ticket.asesor} </Tag>
                     </>
                   }
                 />
