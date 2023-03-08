6feef30f8f4550a144e03380a779bda6ad2dc3ef
Marcela Malaver <marcela9409@gmail.com>
2023-03-08 08:51:54 -0500

    ✨ Mantener la referencia de latitud, longitud y zoom del mapa

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -5,14 +5,15 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoibWFyY2VsYTk0MDkiLCJhIjoiY2xleW91OHgyMDNsazNzcDF0M3A0ZWRpbSJ9.7zPRIRhH605mh8b-V2KrCg";
 
 const initialPoint = {
-  lng: 5,
-  lat: 34,
-  zoom: 10,
+  lng: -73.6041,
+  lat: 2.4402,
+  zoom: 8.09,
 };
 
 export const MapPage = () => {
   const mapDiv = useRef();
   const [myMap, setMyMap] = useState();
+  const [coords, setCoords] = useState(initialPoint);
 
   useEffect(() => {
     var map = new mapboxgl.Map({
@@ -24,8 +25,22 @@ export const MapPage = () => {
     setMyMap(map);
   }, []);
 
+  useEffect(() => {
+    myMap?.on("move", () => {
+      const { lng, lat } = myMap.getCenter();
+      setCoords({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: myMap.getZoom().toFixed(2),
+      });
+    });
+  }, [myMap]);
+
   return (
     <>
+      <div className="info">
+        Lng: {coords.lng} | Lat: {coords.lat} | zoom: {coords.zoom}
+      </div>
       <div ref={mapDiv} className="mapContainer"></div>
     </>
   );
