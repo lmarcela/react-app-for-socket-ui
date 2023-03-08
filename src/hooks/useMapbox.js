747df33c44d8736ea6d560633b10b56f638fc139
Marcela Malaver <marcela9409@gmail.com>
2023-03-08 09:34:29 -0500

    ✨ Agregar marcadores y extraerlos en un callback

--- src/hooks/useMapbox.js
+++ src/hooks/useMapbox.js
@@ -1,5 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
+import { v4 } from "uuid";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoibWFyY2VsYTk0MDkiLCJhIjoiY2xleW91OHgyMDNsazNzcDF0M3A0ZWRpbSJ9.7zPRIRhH605mh8b-V2KrCg";
@@ -7,7 +8,21 @@ mapboxgl.accessToken =
 export const useMapbox = (initialPoint) => {
   const mapRef = useRef();
   const myMap = useRef();
+
+  const markers = useRef({});
+
   const [coords, setCoords] = useState(initialPoint);
+
+  const addMarker = useCallback((ev) => {
+    const { lng, lat } = ev.lngLat;
+
+    const marker = new mapboxgl.Marker();
+    marker.id = v4(); // TODO: si el marcador ya tiene ID
+
+    marker.setLngLat([lng, lat]).addTo(myMap.current).setDraggable(true);
+
+    markers.current[marker.id] = marker;
+  }, []);
 
   useEffect(() => {
     var map = new mapboxgl.Map({
@@ -30,8 +45,13 @@ export const useMapbox = (initialPoint) => {
     });
   }, []);
 
+  useEffect(() => {
+    myMap.current?.on("click", addMarker);
+  }, [addMarker]);
+
   return {
-    mapRef,
+    addMarker,
     coords,
+    mapRef,
   };
 };
