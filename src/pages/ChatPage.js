940455ec8de8b8e950d6d5e1342bc8fece2be3d0
Marcela Malaver <marcela9409@gmail.com>
2023-03-09 14:54:56 -0500

    ✨ Diseño de pagina de chat y sus componentes

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { ChatSelect } from "../components/ChatSelect";
+import { InboxPeople } from "../components/InboxPeople";
+import { Messages } from "../components/Messages";
+
+import "../css/chat.css";
 
 export const ChatPage = () => {
-  return <div>ChatPage</div>;
+  return (
+    <div className="messaging">
+      <div className="inbox_msg">
+        <InboxPeople />
+
+        {!true ? <Messages /> : <ChatSelect />}
+      </div>
+    </div>
+  );
 };
