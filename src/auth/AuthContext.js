a9bc092b7d7a0f163aa01d67e4fb08891eb22540
Marcela Malaver <marcela9409@gmail.com>
2023-03-09 15:08:26 -0500

    ✨ Auth - Provider y Context

---
+++ src/auth/AuthContext.js
@@ -0,0 +1,37 @@
+import React, { createContext, useCallback, useState } from "react";
+
+export const AuthContext = createContext();
+
+const initialState = {
+  uid: null,
+  checking: true,
+  logged: false,
+  name: null,
+  email: null,
+};
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(initialState);
+
+  const login = async (email, password) => {};
+
+  const register = async (nombre, email, password) => {};
+
+  const verificaToken = useCallback(async () => {}, []);
+
+  const logout = () => {};
+
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+        login,
+        register,
+        verificaToken,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
