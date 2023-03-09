5c9b5ae345a186af691a6c2b5839bb05e9d724b5
Marcela Malaver <marcela9409@gmail.com>
2023-03-09 15:13:00 -0500

    ✨ Auth - Provider y Context

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,7 +1,12 @@
+import { AuthProvider } from "./auth/AuthContext";
 import { AppRouter } from "./routers/AppRouter";
 
 function ChatApp() {
-  return <AppRouter />;
+  return (
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  );
 }
 
 export default ChatApp;
