42ad9ca20809b01a095f80ce4b6e6617dc82250d
Marcela Malaver <marcela9409@gmail.com>
2023-03-09 14:36:32 -0500

    ✨ Diseño de login y registro

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -4,13 +4,21 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../pages/LoginPage";
 import { RegisterPage } from "../pages/RegisterPage";
 
+import "../css/login-register.css";
+
 export const AuthRouter = () => {
   return (
-    <Routes>
-      <Route exact path="/login" element={<LoginPage />} />
-      <Route exact path="/register" element={<RegisterPage />} />
+    <div className="limiter">
+      <div className="container-login100">
+        <div className="wrap-login100 p-t-50 p-b-90">
+          <Routes>
+            <Route exact path="/login" element={<LoginPage />} />
+            <Route exact path="/register" element={<RegisterPage />} />
 
-      <Route path="*" element={<Navigate to="/auth/login" />} />
-    </Routes>
+            <Route path="*" element={<Navigate to="/auth/login" />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 };
