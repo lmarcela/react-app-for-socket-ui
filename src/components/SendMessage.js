7bfcdaeb46c47ec462116250b5da016efbdd78e1
Marcela Malaver <marcela9409@gmail.com>
2023-03-13 11:18:33 -0500

    ✨ Enviar un mensaje

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../auth/AuthContext";
+import { ChatContext } from "../context/chat/ChatContext";
+import { SocketContext } from "../context/SocketContext";
 
 export const SendMessage = () => {
+  const [message, setMessage] = useState("");
+  const { socket } = useContext(SocketContext);
+  const { auth } = useContext(AuthContext);
+  const { chatState } = useContext(ChatContext);
+
+  const onChange = ({ target }) => {
+    setMessage(target.value);
+  };
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+
+    if (message.length === 0) {
+      return;
+    }
+    setMessage("");
+
+    socket.emit("personal-message", {
+      from: auth.uid,
+      to: chatState.chatActive,
+      message,
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="type_msg row">
         <div className="input_msg_write col-sm-9">
-          <input type="text" className="write_msg" placeholder="Mensaje..." />
+          <input
+            type="text"
+            className="write_msg"
+            placeholder="Mensaje..."
+            value={message}
+            onChange={onChange}
+          />
         </div>
         <div className="col-sm-3 text-center">
           <button className="msg_send_btn mt-3" type="submit">
