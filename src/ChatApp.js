ff1a05a628dbe841d6cf5a70ca68ace0481cab61
Marcela Malaver <marcela9409@gmail.com>
2023-03-13 08:52:26 -0500

    ✨ Chat Context, Provider y Reducer

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,14 +1,17 @@
 import { AuthProvider } from "./auth/AuthContext";
+import { ChatProvider } from "./context/chat/ChatContext";
 import { SocketProvider } from "./context/SocketContext";
 import { AppRouter } from "./routers/AppRouter";
 
 function ChatApp() {
   return (
-    <AuthProvider>
-      <SocketProvider>
-        <AppRouter />
-      </SocketProvider>
-    </AuthProvider>
+    <ChatProvider>
+      <AuthProvider>
+        <SocketProvider>
+          <AppRouter />
+        </SocketProvider>
+      </AuthProvider>
+    </ChatProvider>
   );
 }
 
---
+++ src/context/chat/ChatReducer.js
@@ -0,0 +1,6 @@
+export const ChatReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
