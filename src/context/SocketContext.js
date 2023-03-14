18f49b9cd747b5eb15d891365f9ae3d6795c8470
Marcela Malaver <marcela9409@gmail.com>
2023-03-14 15:13:20 -0500

    🚀 Deploy to vercel

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -5,7 +5,7 @@ import { useSocket } from "../hooks/useSocket";
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
-  const { socket, online } = useSocket("http://localhost:8080");
+  const { socket, online } = useSocket("https://node-for-socket-io-git-backend-server-maps-app-lmarcela.vercel.app");
 
   return (
     <SocketContext.Provider value={{ socket, online }}>
